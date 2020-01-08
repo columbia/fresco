33cd6baf2caa513995cd003b3163b3c6891215b0
unknown <koh@cs.columbia.edu>
2020-01-08 00:27:38 -0500

    Hopefully correctly implement image-wide MCU permuting

--- native-imagetranscoder/src/main/jni/native-imagetranscoder/jpeg/crypto/jpeg_encrypt.cpp
+++ native-imagetranscoder/src/main/jni/native-imagetranscoder/jpeg/crypto/jpeg_encrypt.cpp
@@ -466,41 +466,52 @@ static void permuteMCUs(
     }
     gen_chaotic_sequence(chaotic_seq, n_blocks, x_0, mu);
 
-    ////
+    int k, curr_block;
+    k = 0;
+    curr_block = k;
+    std::fill(sorted_blocks, sorted_blocks + n_blocks, false);
+    while (k < n_blocks) {
+      if (sorted_blocks[k]) {
+        k += 1;
+        curr_block = k;
+        continue;
+      } else {
+        JBLOCKARRAY mcu_src_rows;
+        JBLOCKARRAY mcu_dst_rows;
+        int src_row_idx = 0;
+        int dst_row_idx = 0;
+        int src_mcu_idx = 0;
+        int dst_mcu_idx = 0;
 
-    //LOGD("permuteMCUs before access_virt_barray");
-    //LOGD("permuteMCUs after access_virt_barray");
+        // figure out where block k is in the image in the context of the 2D image array
+        src_row_idx = k / width;
+        src_mcu_idx = k - src_row_idx * width;
 
-    for (int i = 0; i < n_blocks; i++) {
-      JBLOCKARRAY mcu_src_rows;
-      JBLOCKARRAY mcu_dst_rows;
-      int src_row_idx = 0;
-      int dst_row_idx = 0;
-      int src_mcu_idx = 0;
-      int dst_mcu_idx = 0;
+        // look up where curr_block should go in the context of the 2D image array
+        dst_row_idx = chaotic_seq[curr_block].chaos_pos / width;
+        dst_mcu_idx = chaotic_seq[curr_block].chaos_pos - dst_row_idx * width;
 
+        //LOGD("permuteMCUs src_row_idx=%d, src_mcu_idx=%d, dst_row_idx=%d, dst_mcu_idx=%d", src_row_idx, src_mcu_idx, dst_row_idx, dst_mcu_idx);
 
-      src_row_idx = i / width;
-      src_mcu_idx = i - src_row_idx * width;
+        if (chaotic_seq[curr_block].chaos_pos != k) {
+          mcu_src_rows = (dinfo->mem->access_virt_barray)((j_common_ptr) dinfo, src_coefs[comp_i], src_row_idx, (JDIMENSION) 1, TRUE);
+          mcu_dst_rows = (dinfo->mem->access_virt_barray)((j_common_ptr) dinfo, src_coefs[comp_i], dst_row_idx, (JDIMENSION) 1, TRUE);
 
-      dst_row_idx = chaotic_seq[i].chaos_pos / width;
-      dst_mcu_idx = chaotic_seq[i].chaos_pos - dst_row_idx * width;
+          // mcu_rows[y][x][c]
+          // - the yth vertical block
+          // - the xth horizontal block
+          // - the cth coefficient
+          std::swap(mcu_src_rows[0][src_mcu_idx], mcu_dst_rows[0][dst_mcu_idx]);
 
-      LOGD("permuteMCUs src_row_idx=%d, src_mcu_idx=%d, dst_row_idx=%d, dst_mcu_idx=%d", src_row_idx, src_mcu_idx, dst_row_idx, dst_mcu_idx);
-
-      LOGD("permuteMCUs before access_virt_barray");
-      mcu_src_rows = (dinfo->mem->access_virt_barray)((j_common_ptr) dinfo, src_coefs[comp_i], src_row_idx, (JDIMENSION) 1, TRUE);
-      mcu_dst_rows = (dinfo->mem->access_virt_barray)((j_common_ptr) dinfo, src_coefs[comp_i], dst_row_idx, (JDIMENSION) 1, TRUE);
-      LOGD("permuteMCUs after access_virt_barray");
-
-      // mcu_rows[y][x][c]
-      // - the yth vertical block
-      // - the xth horizontal block
-      // - the cth coefficient
-      std::swap(mcu_src_rows[0][src_mcu_idx], mcu_dst_rows[0][dst_mcu_idx]);
+          sorted_blocks[chaotic_seq[curr_block].chaos_pos] = true;
+          curr_block = chaotic_seq[curr_block].chaos_pos;
+        } else {
+          sorted_blocks[k] = true;
+          k += 1;
+          curr_block = k;
+        }
+      }
     }
-
-    ////
 
     for (int i; i < n_blocks; i++) {
       mpf_clear(chaotic_seq[i].chaos_gmp);
